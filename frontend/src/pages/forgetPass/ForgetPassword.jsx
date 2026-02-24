f9dfef621db766c96b79f90c0c820747c7947472
Doha Saeed <153032586+Doha2127277@users.noreply.github.com>
2026-02-24 21:55:20 +0200

    Fix on ForgetPassword

---
+++ frontend/src/pages/forgetPass/ForgetPassword.jsx
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import "./projectSoft.css";
+import { sendPasswordResetEmail } from "firebase/auth";
+import { auth } from "../../firebase.js";
+
+export default function ForgetPassword() {
+  const [step, setStep] = useState(1);
+  const [email, setEmail] = useState("");
+  const [successMsg, setSuccessMsg] = useState("");
+  const [emailError, setEmailError] = useState("");
+
+  const handleSendCode = async () => {
+    setEmailError("");
+    setSuccessMsg("");
+
+    const trimmedEmail = email.trim().toLowerCase();
+
+    if (!trimmedEmail) {
+      setEmailError("Please Enter Your Email");
+      return;
+    }
+
+    if (!trimmedEmail.includes("@")) {
+      setEmailError("Invalid email format");
+      return;
+    }
+
+    const parts = trimmedEmail.split("@");
+    if (parts.length !== 2) {
+      setEmailError("Invalid email format");
+      return;
+    }
+
+    const domain = parts[1];
+    const blocked = ["gmail.com", "yahoo.com", "hotmail.com", "outlook.com"];
+
+    if (blocked.includes(domain)) {
+      setEmailError("Public emails are not allowed");
+      return;
+    }
+
+    if (!(domain.endsWith(".edu") || domain.endsWith(".edu.eg"))) {
+      setEmailError("Use university email (.edu)");
+      return;
+    }
+
+    try {
+      await sendPasswordResetEmail(auth, trimmedEmail);
+      setSuccessMsg("Reset link sent to your email ");
+      setStep(2);
+    } catch (error) {
+      setEmailError(error.message);
+    }
+  };
+
+  return (
+    <div>
+      <img src="/image copy 2.png" alt="forget password" />
+
+      <div className="forgetPass">
+        {step === 1 && (
+          <>
+            <p>
+              <b>Please Enter university email and we will send you a reset link.</b>
+            </p>
+            <input
+              type="email"
+              placeholder="example@university.edu"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <p className="error">{emailError}</p>
+            <button onClick={handleSendCode}>Get Reset Link</button>
+          </>
+        )}
+
+        {step === 2 && (
+          <>
+            <p className="success">{successMsg}</p>
+            <a href="/">Back to Login</a>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
